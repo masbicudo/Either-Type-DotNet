5a418bf06e882be2f29e54069e380967b1298aa5
masbicudo <masbicudo@gmail.com>
2016-04-25 11:06:42 -0300

    Implemented `EitherUserType` class's `DeepCopy`, `Disassemble`, `Assemble` and `Replace` methods

--- Either-For-NHibernate/EitherUserType.cs
+++ Either-For-NHibernate/EitherUserType.cs
@@ -78,7 +78,7 @@ namespace Either_For_NHibernate
             var either = value as IEither;
 
             if (either == null)
-                throw new Exception("Must pass an Either struct");
+                throw new Exception("Must pass an `Either<...>` struct");
 
             var selectedAlt = either.GetSelectedAlternative();
             var selectedValue = either.Value;
@@ -93,28 +93,79 @@ namespace Either_For_NHibernate
             }
         }
 
+        public object DeepCopy(object value, ISessionImplementor session, object owner)
+        {
+            if (value == null)
+                return null;
+
+            var either = value as IEither;
+
+            if (either == null)
+                throw new Exception("Must pass an `Either<...>` struct");
+
+            var selectedAlt = either.GetSelectedAlternative();
+            var selectedValue = either.Value;
+            var clonedValue = this.propertyTypes[selectedAlt].DeepCopy(selectedValue, session?.EntityMode ?? 0, session?.Factory);
+            var result = Either.Factory.Create(clonedValue, typeof(T));
+            return result;
+        }
+
         public object DeepCopy(object value)
         {
-            // TODO: not sure what to do
-            return value;
+            return this.DeepCopy(value, null, null);
         }
 
+        /// <summary>
+        /// Returns a value that will be stored in the NHibernate cache, when cache is enabled.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="session"></param>
+        /// <returns></returns>
         public object Disassemble(object value, ISessionImplementor session)
         {
-            // TODO: not sure what to do... not many info about this method
-            return value;
+            return this.DeepCopy(value, session, null);
         }
 
+        /// <summary>
+        /// Restores an object that has been stored in NHibernate cache, when cache is enabled.
+        /// </summary>
+        /// <param name="cached"></param>
+        /// <param name="session"></param>
+        /// <param name="owner"></param>
+        /// <returns></returns>
         public object Assemble(object cached, ISessionImplementor session, object owner)
         {
-            // TODO: not sure what to do... not many info about this method
-            return cached;
+            return this.DeepCopy(cached, session, owner);
         }
 
+        /// <summary>
+        /// During merge, replace the existing (target) value in the entity we are merging to
+        ///             with a new (original) value from the detached entity we are merging.
+        /// </summary>
+        /// <param name="original"></param>
+        /// <param name="target"></param>
+        /// <param name="session"></param>
+        /// <param name="owner"></param>
+        /// <returns></returns>
         public object Replace(object original, object target, ISessionImplementor session, object owner)
         {
-            // TODO: not sure what to do
-            return original;
+            if (original == null)
+                return null;
+
+            var either = original as IEither;
+
+            if (either == null)
+                throw new Exception("Must pass an `Either<...>` struct");
+
+            var targetEither = target as IEither;
+            var targetSelectedValue = targetEither?.Value;
+
+            var selectedAlt = either.GetSelectedAlternative();
+            var selectedValue = either.Value;
+            // TODO: don't know what to pass to inner type `Replace` final parameters
+            var mergedValue = this.propertyTypes[selectedAlt].Replace(selectedValue, targetSelectedValue, session, owner, null);
+            var result = Either.Factory.Create(mergedValue, typeof(T));
+            return result;
         }
 
         public string[] PropertyNames => this.propertyNames.ToArray();
